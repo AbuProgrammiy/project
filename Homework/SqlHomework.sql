f087b0f1d22cb5b3c337e6da5305bc95d0cf93ce
AbuProgrammiy <145345550+AbuProgrammiy@users.noreply.github.com>
2024-01-27 07:51:57 +0500

    Update SqlHomework.sql

--- Homework/SqlHomework.sql
+++ Homework/SqlHomework.sql
@@ -1,1 +1,9 @@
+3.1 Exersize
+	a) select title from course where credits=3 and dept_name = 'Comp. Sci.';
+	c) select max(salary) highest_salary from instructor;
+	d) select * from instructor where salary=(select max(salary) from instructor);
 
+3.3 Exersize
+	a) update instructor
+	   set salary=salary/10+salary
+	   where dept_name='Comp. Sci.';
