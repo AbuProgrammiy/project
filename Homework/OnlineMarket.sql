0a95412d1d8ac4e32e8927df71de4c4187bf1757
AbuProgrammiy <145345550+AbuProgrammiy@users.noreply.github.com>
2024-02-01 08:07:38 +0500

    Update OnlineMarket.sql

--- Homework/OnlineMarket.sql
+++ Homework/OnlineMarket.sql
@@ -1,1 +1,37 @@
-Coming Soon)
+--Continuing...
+
+
+
+create table tags(id int,
+				  tag_name varchar(255),
+				  icon text,
+				  created_at timestamptz,
+				  uploated_at timestamptz,
+				 created_by UUID,
+				 updated_by UUID);
+
+create table categories(id UUID,
+					   parent_id UUID,
+					   category_name varchar(255),
+					   category_description text,
+					   icon text,
+					   image_path text,
+					   active boolean,
+					   created_at timestamptz,
+				  	   uploated_at timestamptz,
+				 		created_by UUID,
+				 		updated_by UUID);
+						
+create table staff_accounts(id UUID,
+					 first_name varchar(100),
+					 last_name varchar(100),
+					 phone_number varchar(100),
+					 email varchar(255),
+					 password_hash text,
+					 active boolean,
+					 profile_img text,
+					 registered_at timestamptz,
+					 uploadet_at timestamptz,
+					 created_by UUID,
+					 uploaded_by UUID);
+
